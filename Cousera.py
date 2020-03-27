52e0a2526b5c9c925a946aef49c0d1cb88e1fe66
stunningvian <62731990+stunningvian@users.noreply.github.com>
2020-03-26 22:18:25 -0400

    Add files via upload

---
+++ Cousera.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # capstone project
+# ## For the Coursera Course
+
+# In[3]:
+
+
+import pandas as pd
+import numpy  as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
